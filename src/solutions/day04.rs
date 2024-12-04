07227477a8957d72fffaff10e285c14968e35998
Aske L R Thomsen <aske.l.r.thomsen@gmail.com>
2024-12-04 20:54:32 +0100

    Optimized both parts

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -26,53 +26,58 @@ impl Day4 {
     }
     
     fn check_valid1(cord: (usize, usize), grid: &Vec<Vec<char>>) -> u32 {
-        let (x, y) = cord; 
+        let (x, y) = cord;
+        let word = ['X', 'M', 'A', 'S'];
+        let mut valid = 0;
+    
         let valid_forward = 3 + x < grid[y].len();
-        let valid_backward = (x as i32) - 3 >= 0;
-        let valid_up = (y as i32) - 3 >= 0;
+        let valid_backward = x >= 3;
+        let valid_up = y >= 3;
         let valid_down = 3 + y < grid.len();
-
-        let mut valid = 0; 
-
-        if valid_forward && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y][x + 1], grid[y][x + 2], grid[y][x + 3]).as_str() {
+    
+        if valid_forward 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y][x + i] == ch) {
             valid += 1;
         }
-        
-        if valid_backward && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y][x - 1], grid[y][x - 2], grid[y][x-3]).as_str() {
+    
+        if valid_backward 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y][x - i] == ch) {
             valid += 1;
         }
-        
-        if valid_up && 
-        "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y-1][x], grid[y-2][x], grid[y-3][x]).as_str() {
+    
+        if valid_up 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x] == ch) {
             valid += 1;
         }
-        
-        if valid_down && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y+1][x], grid[y+2][x], grid[y+3][x]).as_str() {
+    
+        if valid_down 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x] == ch) {
             valid += 1;
         }
-        if valid_forward && valid_up && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y-1][x+1], grid[y-2][x+2], grid[y-3][x+3]).as_str() {
+    
+        if valid_forward && valid_up 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x + i] == ch) {
             valid += 1;
         }
-        if valid_forward && valid_down && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y+1][x+1], grid[y+2][x+2], grid[y+3][x+3]).as_str() {
+    
+        if valid_forward 
+        && valid_down && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x + i] == ch) {
             valid += 1;
         }
-        if valid_backward && valid_up && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y-1][x-1], grid[y-2][x-2], grid[y-3][x-3]).as_str() {
+    
+        if valid_backward && valid_up 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x - i] == ch) {
             valid += 1;
         }
-        if valid_backward && valid_down && "XMAS" == format!("{}{}{}{}", 
-        grid[y][x], grid[y+1][x-1], grid[y+2][x-2], grid[y+3][x-3]).as_str() {
+    
+        if valid_backward && valid_down 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x - i] == ch) {
             valid += 1;
         }
-
+    
         valid
     }
+    
 
     fn solver2(&self) -> u32 {
         let mut total = 0; 
