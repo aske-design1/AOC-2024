057cb2fe84c2db07054ba0a7b1570b48a9e26a32
Aske L R Thomsen <aske.l.r.thomsen@gmail.com>
2024-12-05 22:19:05 +0100

    Finished day 5

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -2,107 +2,103 @@
 use super::*;
 
 pub struct Day4 {
-    input: Vec<Vec<char>>
+    input: String
 }
 
 impl Day4 {
     pub fn new(input: &str) -> Self {
-        let splitter = if input.contains("\r\n") { "\r\n" } else { "\n" };
-        let input = input.split(splitter).map(|line| line.chars().collect()).collect();
-        Self { input }
+        //let splitter = if input.contains("\r\n") { "\r\n" } else { "\n" };
+        //let input = input.split(splitter).map(|line| line.as_bytes().to_vec()).collect();
+        Self { input: input.to_string() }
     }
 
     fn solver1(&self) -> u32 {
+        let splitter = if self.input.contains("\r\n") { "\r\n" } else { "\n" };
+        let grid: Vec<&[u8]> = self.input.split(splitter).map(|line| line.as_bytes()).collect();
+
         let mut total = 0; 
-        let grid = &self.input;
         for (i, line) in grid.iter().enumerate() {
             for (j, char) in line.iter().enumerate() {
-                if *char == 'X' {
-                    total += Self::check_valid1((j,i), grid);
+                if *char == b'X' {
+                    total += Self::check_valid1((j,i), &grid);
                 }
             }
         }
         total
     }
-    
-    fn check_valid1(cord: (usize, usize), grid: &Vec<Vec<char>>) -> u32 {
+
+    fn check_valid1(cord: (usize, usize), grid: &Vec<&[u8]>) -> u32 {
         let (x, y) = cord;
-        let word = ['X', 'M', 'A', 'S'];
-        let mut valid = 0;
+        let word = [b'X', b'M', b'A', b'S'];
 
         //u8 that contains bits for valid dirs: 
-        let mut validizer: u8 = 0; 
+        let directions: u8 =  
         //1st bit = forward
-        if 3 + x < grid[y].len() { validizer |= 1 << 0 }
+        (1 << 0) * ((3 + x < grid[y].len()) as u8) +
         //2nd bit = backward
-        if x >= 3 { validizer |= 1 << 1 }
+        (1 << 1) * ((x >= 3) as u8) +
         //3rd bit = up 
-        if y >= 3 { validizer |= 1 << 2 }
-        //4th bit = down 
-        if 3 + y < grid.len() { validizer |= 1 << 3 }
-        
+        (1 << 2) * ((y >= 3) as u8) +
+        //4th bit = down
+        (1 << 3) * ((3 + y < grid.len() ) as u8);
+
         //Forward
-        if validizer & 0b0001 != 0
-        && word.iter().enumerate().all(|(i, &ch)| grid[y][x + i] == ch) 
-        { valid += 1; }
+        1*((directions & 0b0001 != 0
+        && word.iter().enumerate().all(|(i, &ch)| grid[y][x + i] == ch)) as u32)+
         //Backward
-        if validizer & 0b0010 != 0
-        && word.iter().enumerate().all(|(i, &ch)| grid[y][x - i] == ch) 
-        { valid += 1 }
+        1*((directions & 0b0010 != 0
+        && word.iter().enumerate().all(|(i, &ch)| grid[y][x - i] == ch)) as u32)+
         //Up
-        if validizer & 0b0100 != 0
-        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x] == ch) 
-        { valid += 1 }
+        1*((directions & 0b0100 != 0
+        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x] == ch)) as u32)+
         //Down
-        if validizer & 0b1000 != 0
-        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x] == ch) 
-        { valid += 1 }
+        1*((directions & 0b1000 != 0
+        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x] == ch)) as u32)+
         //Up-forward
-        if validizer & 0b0101 == 0b0101 
-        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x + i] == ch) 
-        { valid += 1 }
+        1*((directions & 0b0101 == 0b0101 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x + i] == ch)) as u32)+
         //down-forward
-        if validizer & 0b1001 == 0b1001  
-        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x + i] == ch) 
-        { valid += 1 }
+        1*((directions & 0b1001 == 0b1001  
+        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x + i] == ch)) as u32)+
         //up-backward
-        if validizer & 0b0110 == 0b0110 
-        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x - i] == ch) 
-        { valid += 1 }
+        1*((directions & 0b0110 == 0b0110 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y - i][x - i] == ch)) as u32)+
         //down-backward
-        if validizer & 0b1010 == 0b1010 
-        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x - i] == ch) 
-        { valid += 1 }
-        valid
+        1*((directions & 0b1010 == 0b1010 
+        && word.iter().enumerate().all(|(i, &ch)| grid[y + i][x - i] == ch)) as u32)
     }
     
 
     fn solver2(&self) -> u32 {
+        let splitter = if self.input.contains("\r\n") { "\r\n" } else { "\n" };
+        //let grid: &[u8] = self.input.as_bytes();
+        let grid: Vec<&[u8]> = self.input.split(splitter).map(|line| line.as_bytes()).collect();
         let mut total = 0; 
-        let grid = &self.input;
+        //let len = grid.iter().find(|&ch| *ch == b'\n');
+
         for (i, line) in grid.iter().enumerate().skip(1).take(grid.len() - 2) {
             for (j, char) in line.iter().enumerate().skip(1).take(line.len() - 2) {
-                if *char == 'A' { 
-                    total += Self::check_valid2((j,i), grid) 
+                if *char == b'A' { 
+                    total += Self::check_valid2((j,i), &grid) 
                 }
             }
         }
         total
     }
-    fn check_valid2(cord: (usize, usize), grid: &Vec<Vec<char>>) -> u32 {
+    fn check_valid2(cord: (usize, usize), grid: &Vec<&[u8]>) -> u32 {
         let (x, y) = cord; 
         //Four cases:
         //M   M     S   M    S   S    M   S
         //  A         A        A        A
         //S   S     S   M    M   M    M   S
-        if 'M' == grid[y-1][x-1] && 'M' == grid[y-1][x+1]
-        && 'S' == grid[y+1][x-1] && 'S' == grid[y+1][x+1]
-        || 'M' == grid[y-1][x+1] && 'M' == grid[y+1][x+1] 
-        && 'S' == grid[y-1][x-1] && 'S' == grid[y+1][x-1]
-        || 'M' == grid[y+1][x-1] && 'M' == grid[y+1][x+1]
-        && 'S' == grid[y-1][x-1] && 'S' == grid[y-1][x+1] 
-        || 'M' == grid[y-1][x-1] && 'M' == grid[y+1][x-1]
-        && 'S' == grid[y-1][x+1] && 'S' == grid[y+1][x+1]
+        if b'M' == grid[y-1][x-1] && b'M' == grid[y-1][x+1]
+        && b'S' == grid[y+1][x-1] && b'S' == grid[y+1][x+1]
+        || b'M' == grid[y-1][x+1] && b'M' == grid[y+1][x+1] 
+        && b'S' == grid[y-1][x-1] && b'S' == grid[y+1][x-1]
+        || b'M' == grid[y+1][x-1] && b'M' == grid[y+1][x+1]
+        && b'S' == grid[y-1][x-1] && b'S' == grid[y-1][x+1] 
+        || b'M' == grid[y-1][x-1] && b'M' == grid[y+1][x-1]
+        && b'S' == grid[y-1][x+1] && b'S' == grid[y+1][x+1]
         { 1 } 
         else { 0 }
     }
--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,34 +1,170 @@
 
+use core::num;
+use std::collections::HashMap;
+
 use super::*;
 
 #[allow(dead_code)]
 pub struct Day5 {
-    input: String
+    order: HashMap<i32, Vec<i32>>,
+    numbers: Vec<Vec<i32>> 
 }
 
 impl Day5 {    
     pub fn new(input: &str) -> Self { 
-        //let splitter = if input.contains("\r\n") { "\r\n" } else { "\n" };
-        let input = input.to_string();
-        Self { input }
+        let splitter = if input.contains("\r\n") { "\r\n" } else { "\n" };
+        let mut order = HashMap::new();
+
+        let (to_order, numbers) = 
+        input.split_once("\n\n").unwrap();
+
+        to_order.split(splitter).for_each(|line| {
+            let (first_num, sec_num) = line.split_once("|").unwrap();
+            let (first_num, sec_num) = (first_num.parse().unwrap(), sec_num.parse().unwrap());
+            order.entry(first_num)
+            .and_modify(|vector: &mut Vec<i32>| vector.push(sec_num)).or_insert(vec![sec_num]);
+            //Second number's value in hash will be negative to tell that it should come after
+            order.entry(sec_num)
+            .and_modify(|vector: &mut Vec<i32>| vector.push(-1 * first_num)).or_insert(vec![-1 * first_num]);
+        });
+
+        let numbers = numbers.split(splitter).map(|line| line.split(",").map(|num| num.parse().unwrap()).collect()).collect();
+        Self { order, numbers }
     }
+
+
+    fn solve1(&self) -> u32 {
+        let mut total = 0;
+        for line in self.numbers.iter() {
+            if Self::check_line(line, &self.order) {
+                let len = line.len();
+                //Gonna work as line is uneven
+                total += line[len / 2] as u32;
+            }
+        }
+        total
+    }
+
+    fn solve2(&self) -> u32 {
+        let mut total = 0;
+        for line in self.numbers.iter() {
+            if !Self::check_line(line, &self.order) {
+                //Gonna work as line is uneven
+                total += Self::sort_arr(line.clone(), &self.order);
+            }
+        }
+        total
+    }
+
+    fn sort_arr(mut numbers: Vec<i32>, order: &HashMap<i32, Vec<i32>>) -> u32 {
+        let len = numbers.len();
+        let mut i = 0; 
+        while i < (len / 2) + 1 {
+            let Some(nums_to_cmp) = order.get(&numbers[i]) else { i+=1; continue };
+            let mut j = i;
+            let mut flag = false;
+
+            while j < len {
+                if !nums_to_cmp.contains(&(-1 * numbers[j])) { j+=1; continue; }
+                flag = true; 
+                let mut k = j;
+                while i < k {
+                    // 0     4
+                    // i     j 
+                    // 5 2 4 1 3
+                    //Swap numbers
+                    (numbers[k - 1], numbers[k]) = (numbers[k], numbers[k - 1]); 
+                    k-=1;
+                }
+                i+=1;
+            }
+            if flag { i = 0; continue }
+
+            i+=1;
+        }
+
+
+        numbers[len / 2] as u32
+    }
+
+
+    fn check_line(numbers: &Vec<i32>, order: &HashMap<i32, Vec<i32>>) -> bool {
+        for (i, num_to_compare) in numbers.iter().enumerate() {
+            for (j, other_num) in numbers.iter().enumerate() {
+                if j == i { continue }
+
+                if let Some(nums) = order.get(num_to_compare) {
+                    let mut idx = None;
+                    for (k, num) in nums.iter().enumerate() {
+                        if *num == *other_num {
+                            idx = Some(k);
+                            break;
+                        }
+                    }
+                    if idx.is_none() {
+                        continue;
+                    }
+
+                    if i < j && nums.get(idx.unwrap()).is_some_and(|num: &i32| *num < 0) 
+                    || j < i && nums.get(idx.unwrap()).is_some_and(|num| *num > 0) {
+                        //println!("{num_to_compare} {other_num} -> {}", nums[0]);
+
+                        return false
+                    }
+                }
+            }
+        }
+
+        true
+    }
+
 }
 
 impl Solution for Day5 {
-    fn part1(&self) -> String { 0.to_string() }
-    fn part2(&self) -> String { 0.to_string() }
+    fn part1(&self) -> String { self.solve1().to_string() }
+    fn part2(&self) -> String { self.solve2().to_string() }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TEST: &str = "INPUT HERE";
+    const TEST: &str = 
+"47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
     
     #[test] fn test1() {
-        assert_eq!(Day5::new(TEST).part1(), 0.to_string());
+        let day = Day5::new(TEST);
+
+        assert_eq!(day.part1(), 143.to_string());
     }
     #[test] fn test2() {
-        assert_eq!(Day5::new(TEST).part2(), 0.to_string());
+        assert_eq!(Day5::new(TEST).part2(), 123.to_string());
     }
 }
 
